1513d43794750df5ab90bf7cd17441ff5fe21675
Jose Hernandez <jmhernandeze@gmail.com>
2020-03-23 16:57:31 -0700

    minor cleanup

--- google_download_file.py
+++ google_download_file.py
@@ -16,7 +16,7 @@ project_root = os.path.split(this_file_path)[0]
 path_root = os.path.join(project_root, "data") + '/'
 path_to_metadata = os.path.join(project_root, "metadata") + '/'
 path_to_cadrs = path_root + 'cadrs/'
-
+os.path.join(project_root, 'client_secret.json')
 import datetime
 
 def _getToday():
@@ -24,7 +24,7 @@ def _getToday():
 
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(project_root, 'client_secret.json'), scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("training_data_updated").sheet1
